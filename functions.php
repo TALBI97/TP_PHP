def2462223c5778e45f06d847d4c94aa8c5f2379
chaymae <chaimaetalbi2016@gmail.com>
2021-05-21 14:43:20 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+
+function enregistrerFichierEnvoye(array $infoFichier): string
+{
+    // strval(): Récupère la valeur d'une variable, au format chaîne
+    //time():Retourne le timestamp
+    // on récuppere la date et l'heure en string dans $timestamp.
+    $timestamp = strval(time());
+
+    // pathinfo(): Retourne des informations sur un chemin système
+    // basename(): Retourne le nom de la composante finale d'un chemin
+    $extension = pathinfo(basename($infoFichier["name"]), PATHINFO_EXTENSION);
+    //création du nom de l'image en vue de le sauveguardé
+    $nomDuFichier = 'produit_' . $timestamp . '.' . $extension;
+    //création du lien de stockage de l'image 
+     $dossierStockage = __DIR__ . '/uploads/';
+
+    //on vérifie si le dossier ou l'on va stocké l'image existe
+
+    if (file_exists($dossierStockage) === false)
+    {
+
+        // sinon on crée le dossier
+        mkdir($dossierStockage);
+    }
+    // on copie l'image dans son nouveau repertoire
+    move_uploaded_file($infoFichier["tmp_name"], $dossierStockage . $nomDuFichier);
+    // return '../uploads/' . $nomDuFichier;
+    // on renvoi le lien vers le fichier
+    return '/produit-crud/produit-crud/uploads/' . $nomDuFichier;
+}
+function onVaRediriger(string $path)
+{
+
+    // on modifie le header (l'URL) pour redirigé
+    header('LOCATION: .' . $path);
+    // on sort de la fonction
+    die();
+}
